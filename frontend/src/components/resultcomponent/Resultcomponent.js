4a03daa3045fbce26f3d034021c8e06f276fc2c7
Pascal-Mac <pascalsbakker@gmail.com>
2018-11-10 12:17:41 -0500

    Basic Result Component

---
+++ frontend/src/components/resultcomponent/Resultcomponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Resultcomponent.css'
+export default class Resultcomponent extends Component
+{
+    render(){
+        return(
+            <div className="component-Resultcomponent">
+
+            </div>
+        )
+    }
+}
